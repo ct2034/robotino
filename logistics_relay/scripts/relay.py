736eded07904e13c923f7c324186a43f39e8995d
cch <christian.henkel@ipa.fraunhofer.de>
2016-04-14 21:48:40 +0200

    pose and map

--- logistics_relay/scripts/relay.py
+++ logistics_relay/scripts/relay.py
@@ -4,6 +4,15 @@ import rospy
 import tf
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
+from nav_msgs.msg import OccupancyGrid
+
+import numpy as np
+import png
+import random
+import io
+
+map_frame = "/map"
+link_frame = "/base_link"
 
 
 def pubGoal(x, y, th):
@@ -16,7 +25,7 @@ def pubGoal(x, y, th):
     ps.pose.position.z = 0.0
     ps.pose.orientation = tf.transformations.quaternion_from_euler(0, 0, th)
 
-    pub.publish(ps)
+    pubGoalPose.publish(ps)
 
 
 def callbackGoal(data):
@@ -32,11 +41,62 @@ def callbackGoal(data):
     pubGoal(data.x, data.y, data.z)
 
 
+def callbackMap(data):
+    rospy.loginfo("RELAY recieved map")
+    na = np.array(data.data)
+    p = np.reshape(na, (data.info.height, data.info.width), 'C')
+    print p
+
+    # rand = str(random.randrange(0, 999999))
+    # path = '/tmp/' + rand + '.png'
+    # f = open(path, 'wb')
+    f = io.BytesIO()
+    w = png.Writer(data.info.width, data.info.height, greyscale=True)
+    w.write(f, (p / 100 + 1) * 100)
+
+    # rospy.loginfo("RELAY map saved to " + path)
+
+    pnghex = ''.join(["%02X" % ord(x) for x in f.getvalue()]).strip()
+
+    f.close()
+
+    rospy.loginfo("hex length: " + str(len(pnghex)))
+    # rospy.loginfo(pnghex)
+
+
 if __name__ == '__main__':
     rospy.init_node('relay', anonymous=True)
+    rospy.loginfo("RELAY init")
 
     rospy.Subscriber("logistics_goal", Point, callbackGoal)
-    pub = rospy.Publisher(
+    rospy.Subscriber("map", OccupancyGrid, callbackMap)
+    pubGoalPose = rospy.Publisher(
         'move_base_simple/goal', PoseStamped, queue_size=10)
+    pubCurrentPose = rospy.Publisher(
+        'logistics_pose', Point, queue_size=10)
 
-    rospy.spin()
+    rate = rospy.Rate(1)
+    listener = tf.TransformListener()
+
+    while not rospy.is_shutdown():
+        rospy.loginfo("RELAY pose lookup")
+
+        while(
+            (not listener.frameExists(map_frame)) or
+            (not listener.frameExists(link_frame)) and
+            (not rospy.is_shutdown())
+        ):
+            # rospy.loginfo("RELAY waiting for position")
+            rate.sleep()
+
+        t = listener.getLatestCommonTime(link_frame, map_frame)
+        position, quaternion = listener.lookupTransform(
+            link_frame, map_frame, t)
+
+        rospy.loginfo("RELAY pose " + str((position, quaternion)))
+
+        p = Point()
+
+        pubCurrentPose.publish(p)
+
+        rate.sleep()
