3f24a501aa7efb4c1876de479695f523a08e7734
Poulastya Mukherjee <poulastya.mukherjee@ipa.fraunhofer.de>
2017-02-22 09:59:53 +0100

    Updated Python Script

--- RemoteAccessScript.py
+++ RemoteAccessScript.py
@@ -43,20 +43,24 @@ def md5checksum(str1,str2):
     else:
         return False
 
-def createDocker(image):
+def createDocker(image,ip):
     print "Docker file does not exist. Creating docker file..."
     os.mkdir(image)
-    tmppath = os.getcwd() + '/image'
+    tmppath = os.getcwd() + '/' + image
+    print tmppath
     os.chdir(tmppath)
     file = open('Dockerfile','w')
     file.write("FROM ros:kinetic-robot\n")
     file.write("\n")
     file.write("#ros-kinetic-navigation\n")
-    file.write("RUN apt-get update")
+    file.write("RUN apt-get update\n")
     file.write("RUN apt-get install -y \\ \n")
     file.write("\tros-kinetic-navigation \\ \n")
     file.write("\tros-kinetic-roslint \\ \n")
-    file.write("\tros-kinetic-image*\n")
+    file.write("\tros-kinetic-image* \\ \n")
+    file.write("\tros-kinetic-teleop-twist-keyboard \\ \n")
+    file.write("\tros-kinetic-gmapping\n")
+    
     file.write("\n")
     file.write("EXPOSE 22 80\n")
     file.write("CMD export ROS_MASTER_URI=http://172.17.0.1:11311\n")
@@ -64,7 +68,7 @@ def createDocker(image):
     file.close()
     tarcode = "tar zcf Dockerfile.tar.gz Dockerfile"
     subprocess.call(tarcode,shell=True)
-    buildcode = "curl -v -X POST -H " + '"Content-Type:application/tar"'+ " --data-binary '@Dockerfile.tar.gz' http://" + ip + ":2375/build?t=" + image
+    buildcode = "curl -v -X POST -H " + '"Content-Type:application/tar"'+ " --data-binary '@Dockerfile.tar.gz' http://" + ip + ":2375/build?t=" + image + md5.new(image).hexdigest()
     subprocess.call(buildcode ,shell=True)       
 #os.system("ls -l")
 #subprocess.call(["ls", "-l"])
@@ -72,34 +76,45 @@ def createDocker(image):
 try:
     ip = sys.argv[1]
 except:
-    print "argument not entered!exiting script"
+    print "IP address not entered!exiting script"
+    exit()
+    
+try:
+    image = sys.argv[2]
+except:
+    print "Image name not entered!exiting script"
     exit()
 
 dockercmd_img = "docker -H tcp://" + ip + ":2375 images"
 #dockercmd = "ls -l"
 subprocess.call(dockercmd_img, shell=True)
-image = raw_input("Enter docker image name from list:")
-dockercmd_getimages = "curl -v http://" + ip + ":2375/images/search?term=" + image + " | jq '.[] | .name'"
-
-
+#image = raw_input("Enter docker image name from list:")
+dockercmd_getimages = "curl -v http://" + ip + ":2375/images/search?term=" + image + " | jq '.[] | .name' >> tmpfile"
+subprocess.call(dockercmd_getimages,shell=True)
+pathtmpfile = os.path.abspath("tmpfile")
+tmpfile = open(pathtmpfile,"r")
+img_nm = tmpfile.read()
+img_nm = img_nm[1:-2]
+print img_nm
+os.remove(pathtmpfile)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 0))
 ros_ip = s.getsockname()[0]
 
 
-dockercmd_runimg = "docker -H tcp://" + ip + ":2375 run -it --rm -u " + '"$(id -u):$(id -g)"' + " -v $HOME/.ros:/.ros -v $HOME/.config/catkin:/.config/catkin:ro -v $(pwd):$(pwd) -w $(pwd) " + image
+dockercmd_runimg = "docker -H tcp://" + ip + ":2375 run -it --rm -u " + '"$(id -u):$(id -g)"' + " -v $HOME/.ros:/.ros -v $HOME/.config/catkin:/.config/catkin:ro -v $(pwd):$(pwd) -w $(pwd) " + image + md5.new(image).hexdigest()
 docker_exec_str = "docker -H tcp://" + ip + ":2375 " + "exec $(docker -H tcp://" + ip + ":2375 ps -q) "
 dockercmd_getconid = docker_exec_str + "ls"
 dockercmd_rosmaster = docker_exec_str + "export ROS_MASTER_URI=http://" + ip + ":11311"
 dockercmd_rosip = docker_exec_str + "export ROS_IP="+ros_ip
 dockercmd_teleop = docker_exec_str + ""
 
-var = md5checksum(image,dockercmd_getimages)
+var = md5checksum(image,img_nm)
 if (var == False):
-    createDocker(image)
+    createDocker(image,ip)
     
 cmds = [dockercmd_runimg]
-cmds.append(dockercmd_getconid)
+#cmds.append(dockercmd_getconid)
 #cmds.append(dockercmd_rosmaster)
 #cmds.append(dockercmd_rosip)
 #print cmds[0]
