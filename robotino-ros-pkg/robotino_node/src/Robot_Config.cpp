d9344bb2b9a3cd33d2b3d1909781a3ccaf5b07be
proffessor.x <ali_haider7862002@yahoo.com>
2018-06-27 13:18:15 +0200

    working on env var

--- robotino-ros-pkg/robotino_node/src/Robot_Config.cpp
+++ robotino-ros-pkg/robotino_node/src/Robot_Config.cpp
@@ -20,41 +20,7 @@ public:
 
   void callback(std_msgs::String data)
   {
-   geometry_msgs::PoseStamped goal;
-   goal.header.frame_id = "map";
-   goal.header.stamp = ros::Time::now();
- 	
-	if(data.data == "Middle2")
-	{
-		goal.pose.position.x = -9.187704; 
-		goal.pose.position.y = 3.86997842;
-		goal.pose.orientation.z = -0.0349923222;
-		goal.pose.orientation.w = 0.999387;
-    	pub_.publish(goal);
-   	ROS_INFO("Sending goal");
-	} else if (data.data == "Gate")
-	{
-		goal.pose.position.x = -14.920475; 
-		goal.pose.position.y = 6.157745;
-		goal.pose.orientation.z = -0.731415722;
-		goal.pose.orientation.w = 0.681931;
-    	pub_.publish(goal);
-   	ROS_INFO("Sending Outside goal");
-	} else if (data.data == "Home")
-	{
-		goal.pose.position.x = 0.116755; 
-		goal.pose.position.y = 0.044355;
-		goal.pose.orientation.z = -0.12338;
-		goal.pose.orientation.w = 0.9923;
-    	pub_.publish(goal);
-   	ROS_INFO("Sending Home goal");
-	} else if(data.data == "Middle")
-	{
-		goal.pose.position.x = -7.38350582; 
-		goal.pose.position.y = 4.717759609;
-		goal.pose.orientation.z = 0.01228957;
-		goal.pose.orientation.w = 0.99992448;
-    	pub_.publish(goal);
+      	pub_.publish(goal);
    	ROS_INFO("Sending Middle goal");
 	}
   }
